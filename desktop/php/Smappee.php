2043f01cf4a949bbccd671d19ac514307ff9fa95
Alien@lv426 <goupilpierre@gmail.com>
2018-01-16 20:46:26 +0100

    ADDED plugin conf page now displays real appliances

--- desktop/php/Smappee.php
+++ desktop/php/Smappee.php
@@ -6,7 +6,7 @@ $plugin = plugin::byId('Smappee');
 sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
 ?>
-
+<?php include_file('desktop', 'Smappee', 'css', 'Smappee'); ?>
 <div class="row row-overflow">
     <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
@@ -41,15 +41,23 @@ foreach ($eqLogics as $eqLogic) {
   <legend><i class="fa fa-table"></i> {{Mes Smappees}}</legend>
 <div class="eqLogicThumbnailContainer">
     <?php
-foreach ($eqLogics as $eqLogic) {
-	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-	echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
-	echo "<br>";
-	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
-	echo '</div>';
-}
-?>
+        $command = escapeshellcmd('../../resources/demond/jeedom/Smappee.py');
+        $output = shell_exec($command);
+
+        $tempdir = sys_get_temp_dir();
+        $json = file_get_contents($tempdir . '/Smappee.json');
+        $json_data = json_decode($json, true);
+        $opacity = '' ;
+
+        foreach ($json_data as $appliance) {
+            $appliance_name = $appliance['name'];
+
+            echo '<span class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $appliance['id'] . ' smappeeAppliance">';
+            echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
+            echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $appliance_name . '</span>';
+            echo '</span>';
+        }
+    ?>
 </div>
 </div>
 
