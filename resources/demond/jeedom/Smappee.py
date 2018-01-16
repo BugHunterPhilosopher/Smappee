f595ac76a48d4f3a0740f374c348d0796b5d5525
Alien@lv426 <goupilpierre@gmail.com>
2018-01-16 09:17:47 +0100

    code CLEAN-UP

--- resources/demond/jeedom/Smappee.py
+++ resources/demond/jeedom/Smappee.py
@@ -7,15 +7,10 @@ def main():
     s = smappy.Smappee('AlienQueen', 'pniT248yJ1')
     s.authenticate('AlienQueen', 'Fassbinder#33')
     locs = s.get_service_locations()
-    #print(locs)
     infos = locs['serviceLocations'][0]['serviceLocationId']
-    #print(infos)
     all = s.get_service_location_info(infos)
-    #print(all)
     appliances = all['appliances']
-    #print("appliances: " + appliances)
     for appliance in appliances:
-        #print(appliance)
         print('name: %s' % appliance['name'])
         print('id: %s' % appliance['id'])
 
