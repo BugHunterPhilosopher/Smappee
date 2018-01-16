d30832a8ffa193308606ca19c886a81c53a0a81a
Alien@lv426 <goupilpierre@gmail.com>
2018-01-16 19:47:15 +0100

    ADDED Json written to file

--- resources/demond/jeedom/Smappee.py
+++ resources/demond/jeedom/Smappee.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
+import io
+import json
 import pytz
 import smappy
+import tempfile
 
 def main():
     s = smappy.Smappee('AlienQueen', 'pniT248yJ1')
     s.authenticate('AlienQueen', 'Fassbinder#33')
     locs = s.get_service_locations()
-    infos = locs['serviceLocations'][0]['serviceLocationId']
-    all = s.get_service_location_info(infos)
+    infos = locs['serviceLocations'][0]
+    locs = infos['serviceLocationId']
+    all = s.get_service_location_info(locs)
     appliances = all['appliances']
-    for appliance in appliances:
-        print('name: %s' % appliance['name'])
-        print('id: %s' % appliance['id'])
+
+    dirpath = tempfile.gettempdir()
+
+    with io.open(dirpath + '/Smappee.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(appliances, ensure_ascii=False))
+
+    #for appliance in appliances:
+    #    print('name: %s' % appliance['name'])
+    #    print('id: %s' % appliance['id'])
 
 
 if __name__ == '__main__':
